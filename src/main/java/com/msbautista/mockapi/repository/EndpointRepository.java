85049dd5423468548df9b79b8ab9d8244a1431a4
Maicol Bautista <mbautista@masiv.com>
2022-04-16 16:13:42 -0500

    Add repository for "Endpoint" model

--- src/main/java/com/msbautista/mockapi/repository/EndpointRepository.java
+++ src/main/java/com/msbautista/mockapi/repository/EndpointRepository.java
@@ -4,6 +4,11 @@ import com.msbautista.mockapi.entity.Endpoint;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EndpointRepository extends CrudRepository<String, Endpoint> {
+public interface EndpointRepository extends CrudRepository<Endpoint, String> {
+
+    Optional<Endpoint> findByName(String name);
+
 }
